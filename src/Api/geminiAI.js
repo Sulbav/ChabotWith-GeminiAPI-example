7e3804c0258f6021b821d9f6381d4339a7b223d9
Abraham Sulbaran <abrahamsulbaram@gmail.com>
2025-02-01 01:50:19 -0400

    algunos cambios al .env y .env.example

--- src/Api/geminiAI.js
+++ src/Api/geminiAI.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
 	model: "gemini-1.5-flash",
-	systemInstruction: "Eres un asistente y hablas Español",
+	systemInstruction: import.meta.env.VITE_INSTRUCTION_API,
 });
 
 export const chat = model.startChat({
